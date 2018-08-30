d57c654f0d7775391002f4f7c3d7904d0bda886c
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 20:02:04 -0500

    Demonstrate basic data types

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,13 @@
 public class HelloWorld {
 	// This is a single comment line
 	public static void main(String[] args){
+		int myAge = 42;
+		char mySex = 'M';
+		double myHeight = 5.7;
 		System.out.println("Hello CIS 2323");
+		System.out.println("I am " + myAge + " years old.");
+		System.out.println("I am a " + mySex);
+		System.out.print("My height is " + myHeight + "\n");
 		/* 
 		This is a multi-line comment 
 		System.out.println("Got this far"); */
