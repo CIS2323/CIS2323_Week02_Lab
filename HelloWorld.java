ec2d553bfd337ba3e0bd7b0859766329da644614
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-08-29 19:36:57 -0500

    Added examples of comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
+/**
+ * @author Bobby Rockers
+ * This is a very basic hello world class used as an exampl
+ * @param none
+ */
 public class HelloWorld {
+	// This is a single comment line
 	public static void main(String[] args){
 		System.out.println("Hello CIS 2323");
+		/* 
+		This is a multi-line comment 
+		System.out.println("Got this far"); */
 	}
 }
